809e98c388123904451d2c83caef6476c54f7ea6
theawakener0 <wakeupwithme000@gmail.com>
2026-06-17 21:22:25 +0300

    ADDED repl

---
+++ repl/repl.go
@@ -0,0 +1,45 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/theawakener0/zod/lexer"
+	tk "github.com/theawakener0/zod/token"
+)
+
+const PROMPT = "\x1b[0;32m>>\x1b[0m "
+
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		
+		line := scanner.Text()
+		
+		switch line {
+		case "/clear":
+			fmt.Printf("\x1b[2J\x1b[H")
+			continue
+		case "/exit":
+			os.Exit(0)
+		}
+
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != tk.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+
+	}
+}
+
